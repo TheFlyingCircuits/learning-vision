Some refactoring, fixes to coordinate system, and now printing rotation vectors

pipelines/cameraCalibration.py
@@ -41,7 +41,7 @@ def initCamera():
 def initNetworkTables():
     networkTables = NetworkTableInstance.getDefault()
 
-    onRobot = False
+    onRobot = True
     if (onRobot):
         print("Starting CircuitVision in Robot Mode")
         networkTables.startClient4("wpilibpi")
@@ -54,19 +54,148 @@ def initNetworkTables():
     return networkTables
 
 
+class CameraCalibrator:
+
+    def __init__(self, imageShape, chessboardPointsPerRow, chessboardPointsPerCol, distanceBetweenCorners=1):
+        # record image dimensions for later
+        # we us numpy convention shape = (rows, cols) = (height, width)
+        self.imageShape = imageShape
+
+        # A "chessboard point" is a place where two black squares touch
+        patternCols = chessboardPointsPerRow # each row has all columns
+        patternRows = chessboardPointsPerCol # each column has all rows
+        self.patternShape = (patternCols, patternRows)
+        self.distanceBetweenCorners = distanceBetweenCorners
+
+        # generate the coordinates of each chessboard corner
+        # as viewed from the chessboard's frame of reference
+        # (top left corner as origin, x axis along the rows, y axis along the columns)
+        # TODO: understand why the distance between corners isn't really necessary.
+        localChessBoardCoordinates = []
+        for r in range(patternRows):
+            for c in range(patternCols):
+                x = c * distanceBetweenCorners
+                y = r * distanceBetweenCorners
+                z = 0
+                localChessBoardCoordinates.append((x, y, z))
+        self.localChessBoardCoordinates = np.array(localChessBoardCoordinates, dtype=np.float32)
+
+        # init arrays for keeping track of point correspondences
+        self.imageCordsOfCorners = []
+        self.correspondingWorldPoints = []
+
+        # init the outputs (default to some already taken values)
+        self.cameraMatrix = None
+        self.distortionCoefficients = None
+        self.rmsReprojectionError = None
+
+    
+    def findCorners(self, greyscaleImage):
+        patternFound, cornerCords = cv.findChessboardCorners(greyscaleImage, patternSize=self.patternShape)
+        if (patternFound):
+            return cornerCords
+        else:
+            return None
+
+
+    def addMeasurement(self, greyscaleImage):
+        # refine corners, then add the correspondences
+        cornerCords = self.findCorners(greyscaleImage)
+        
+        # just copying docs, idk what any of this means
+        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        refinedCornerCords = cv.cornerSubPix(greyscaleImage, cornerCords, (11,11), (-1,-1), criteria)
+
+        self.imageCordsOfCorners.append(refinedCornerCords)
+        self.correspondingWorldPoints.append(self.localChessBoardCoordinates)
+
+        return refinedCornerCords
+
+    def updateCalibration(self):
+        if (len(self.correspondingWorldPoints) == 0):
+            self.setDefaultCalibration()
+            return
+        
+        threeDeePoints = self.correspondingWorldPoints
+        twoDeeProjections = self.imageCordsOfCorners
+        calibrationOutputs = cv.calibrateCamera(threeDeePoints, twoDeeProjections, self.imageShape[::-1], None, None)
+
+        # unpack calibration outputs
+        self.rmsReprojectionError = calibrationOutputs[0]
+        self.cameraMatrix = calibrationOutputs[1]
+        self.distortionCoefficients = calibrationOutputs[2]
+
+        # not used, but here incase I want them later.
+        rotationVecs = calibrationOutputs[3]
+        translationVecs = calibrationOutputs[4]
+
+    def setDefaultCalibration(self):
+        # default values based on previously taken measurements
+        fx = 922.404080591854 # focal length measured in terms of the width of a pixel
+        fy = 917.761755502239 # focal length measured in terms of the height of a pixel
+        cx = 605.0506860423618 # the column in the image that's aligned with the optical axis
+        cy = 405.32112366927157 # the row in the image that's aligned with the optical axis
+        cameraMatrix = np.array([ [fx, 0, cx],
+                                  [0, fy, cy],
+                                  [0, 0, 1]
+                                ], dtype=np.float32)
+        
+        rmsReprojectionError = 0.3495341197583
+        distortionCoeffs = np.array([0, 0, 0, 0, 0], dtype=np.float32)
+
+        self.cameraMatrix = cameraMatrix
+        self.rmsReprojectionError = rmsReprojectionError
+        self.distortionCoefficients = distortionCoeffs
+    
+    def publishToNetworkTables(self):
+
+        if not(hasattr(self, 'networkTable')):
+            networkTables = NetworkTableInstance.getDefault()
+            self.networkTable = networkTables.getTable("cameraCalibration")
+
+            # maybe a calibration object is itself an input to a networking class instead?
+            # that feels like bettern seperation of concerns, but this feels like better
+            # encapsulaiton.
+            self.captureCountPub = self.networkTable.getDoubleTopic("captureCount").publish()
+            self.centerXPub = self.networkTable.getDoubleTopic("centerX").publish()
+            self.centerYPub = self.networkTable.getDoubleTopic("centerY").publish()
+            self.focalXPub = self.networkTable.getDoubleTopic("focalX").publish()
+            self.focalYPub = self.networkTable.getDoubleTopic("focalY").publish()
+            self.rmsErrorPub = self.networkTable.getDoubleTopic("rmsReprojectError").publish()
+
+        self.captureCountPub.set(len(self.correspondingWorldPoints))
+
+        if (self.cameraMatrix is None):
+            self.centerXPub.set(0)
+            self.centerYPub.set(0)
+            self.focalXPub.set(0)
+            self.focalYPub.set(0)
+            self.rmsErrorPub.set(0)
+            return
+        
+        
+        self.centerXPub.set(self.cameraMatrix[0, 2])
+        self.centerYPub.set(self.cameraMatrix[1, 2])
+        self.focalXPub.set(self.cameraMatrix[0, 0])
+        self.focalYPub.set(self.cameraMatrix[1, 1])
+        self.rmsErrorPub.set(self.rmsReprojectionError)
+
+
+
 framePutter = None
 
 def main():
     networkTables = initNetworkTables()
     camera, frameWidth, frameHeight, targetFPS = initCamera()
 
-    # setup button to switch between calibration mode and AR mode
+    # create a button to switch between calibration mode and AR mode
     networkTable = networkTables.getTable("cameraCalibration")
     modeButtonPub = networkTable.getBooleanTopic("captureMode").publish()
     modeButtonSub = networkTable.getBooleanTopic("captureMode").subscribe(True)
     modeButtonPub.set(True) # True = is calibrating, False = visualizing (not calibrating)
+    modeButtonSub.readQueue() # read back the value we just published so we don't detect a change in mode on the first loop
 
-    # setup button to capture calibration data
+    # create a button to capture calibration data
     captureButtonPub = networkTable.getBooleanTopic("capture").publish()
     captureButtonSub = networkTable.getBooleanTopic("capture").subscribe(False)
     captureButtonPub.set(False)
@@ -81,14 +210,6 @@ def main():
     print("numpyVersion:", np.__version__)
     # print("CV Build:", cv.getBuildInformation())
 
-    # init publishers for calibration info:
-    captureCountPub = networkTable.getDoubleTopic("captureCount").publish()
-    centerXPub = networkTable.getDoubleTopic("centerX").publish()
-    centerYPub = networkTable.getDoubleTopic("centerY").publish()
-    focalXPub = networkTable.getDoubleTopic("focalX").publish()
-    focalYPub = networkTable.getDoubleTopic("focalY").publish()
-    rmsErrorPub = networkTable.getDoubleTopic("rmsReprojectError").publish()
-
     # init publishers for tag info
     tagXPub = networkTable.getDoubleTopic("tagXmeters").publish()
     tagYPub = networkTable.getDoubleTopic("tagYmeters").publish()
@@ -97,29 +218,13 @@ def main():
     freedomYPub = networkTable.getDoubleTopic("tagYinches").publish()
     freedomZPub = networkTable.getDoubleTopic("tagZinches").publish()
 
-    # init variables for storing calibration info
-    cameraMatrix = None
-    distortionFactors = None
-    imageCordsOfCorners = []
-    correspondingWorldPoints = []
+    rotationXPub = networkTable.getDoubleArrayTopic("xHatCords").publish()
+    rotationYPub = networkTable.getDoubleArrayTopic("yHatCords").publish()
+    rotationZPub = networkTable.getDoubleArrayTopic("zHatCords").publish()
 
-    # A "chessboard point" is a place where two black squares touch
-    chessboardPointsPerRow = 9
-    chessboardPointsPerCol = 6
-    patternCols = chessboardPointsPerRow # each row has all columns
-    patternRows = chessboardPointsPerCol # each col as all rows
-    patternShape = (patternCols, patternRows)
-    distanceBetweenCornersMeters = 1 #3/100
-    localChessBoardCoordinates = []
-    for r in range(patternRows):
-        for c in range(patternCols):
-            x = c * distanceBetweenCornersMeters
-            y = r * distanceBetweenCornersMeters
-            z = 0
-            localChessBoardCoordinates.append((x, y, z))
-    localChessBoardCoordinates = np.array(localChessBoardCoordinates, dtype=np.float32)
     
     # start grabbing frames
+    cameraCalibrator = CameraCalibrator((frameHeight, frameWidth), 9, 6)
     while (True):
         # get the next frame from the camera, and convert to greyscale
         _, frame = camera.read()
@@ -133,41 +238,28 @@ def main():
             # auto toggle off after click
             captureButtonPub.set(False)
 
+
+
+        # reset calibration data when switching into calibration mode
         if (modeButtonChanged and isCaptureMode):
-            imageCordsOfCorners = []
-            correspondingWorldPoints = []
-        elif (isCaptureMode): # in the middle of calibration mode
-            # look for chessboard in the frame
-            patternFound, corners = cv.findChessboardCorners(greyscale, patternSize=patternShape)
+            cameraCalibrator = CameraCalibrator((frameHeight, frameWidth), 9, 6)
+        # add calibration data while in calibration mode
+        elif (isCaptureMode):
+            corners = cameraCalibrator.findCorners(greyscale)
 
-            if (patternFound):
-                # just copying docs
-                criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-                refinedCorners = cv.cornerSubPix(greyscale, corners, (11,11), (-1,-1), criteria)
-
-                canvas = np.copy(frame)
-                cv.drawChessboardCorners(canvas, patternShape, refinedCorners, patternWasFound=True)
-                framePutter.putFrame(canvas)
+            if (corners is None):
+                framePutter.putFrame(frame)
+            elif (captureButtonClicked):
+                cameraCalibrator.addMeasurement(greyscale)
             else:
                 canvas = np.copy(frame)
-                cv.drawChessboardCorners(canvas, patternShape, corners, patternWasFound=False)
-                framePutter.putFrame(canvas)
-
-            # detect a click of the capture button on the dashboard.
-            if (captureButtonClicked and patternFound):
-                imageCordsOfCorners.append(refinedCorners)
-                correspondingWorldPoints.append(localChessBoardCoordinates)
+                cv.drawChessboardCorners(canvas, cameraCalibrator.patternShape, corners, True)
+        # update intrinsics when switching out of calibration mode
         elif (modeButtonChanged and not(isCaptureMode)):
-            # transition into AR mode. Update camera calibration info
-            rmsReprojectError, cameraMatrix, distortionFactors, rotationVecs, translationVecs = cv.calibrateCamera(correspondingWorldPoints, imageCordsOfCorners, greyscale.shape[::-1], None, None)
-
-            centerXPub.set(cameraMatrix[0, 2])
-            centerYPub.set(cameraMatrix[1, 2])
-            focalXPub.set(cameraMatrix[0, 0])
-            focalYPub.set(cameraMatrix[1, 1])
-            rmsErrorPub.set(rmsReprojectError)
+            cameraCalibrator.updateCalibration()
         elif (not(isCaptureMode)):
-            x, y, z = testOpenCVImpl(frame, cameraMatrix)
+            translationVector, rotationMatrix = testOpenCVImpl(frame, cameraCalibrator.cameraMatrix)
+            x, y, z = translationVector
 
             # only show out to millimeters/thousandths of an inch.
             # anything more than that is probably overkill / just noise.
@@ -179,21 +271,17 @@ def main():
             freedomYPub.set(round(inchesPerMeter * y, 3))
             freedomZPub.set(round(inchesPerMeter * z, 3))
 
+            rotationXPub.set(rotationMatrix[:, 0])
+            rotationYPub.set(rotationMatrix[:, 1])
+            rotationZPub.set(rotationMatrix[:, 2])
 
-        captureCountPub.set(len(correspondingWorldPoints))
-        # captureButtonPub.set(False)
+        cameraCalibrator.publishToNetworkTables()
 
 
-
-
-
-def testOpenCVImpl(image, cameraMatrix):
+def findTagCorners(image):
     # https://docs.opencv.org/4.6.0/d5/dae/tutorial_aruco_detection.html
-    # tagFamily = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_36h11)
-    tagFamily = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_16h5)
-    tagWidthInches = 6 # 6.5 for 36h11, 6 for 16h5
-    metersPerInch = 0.0254
-    tagWidthMeters = tagWidthInches * metersPerInch
+    tagFamily = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_36h11)
+    # tagFamily = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_APRILTAG_16h5)
 
     # don't use the default of no corner refinement! we want precision!
     # https://docs.opencv.org/4.6.0/d1/dcd/structcv_1_1aruco_1_1DetectorParameters.html
@@ -203,24 +291,63 @@ def testOpenCVImpl(image, cameraMatrix):
     # corners is a list of 4x2 numpy arrays
     # within each numpy array, there are 4 corners (xCord(column) followed by yCord(row)), ordered clockwise
     # [topLeft, topRight, bottomRight, bottomLeft] (source is 1st link, not mentioned in function docs (ugh))
+    # 
     # experiment differs from documentation.
     # experiment says corner order is [bottomRight, bottomLeft, topLeft, topRight]
+    # will look into fixing this later. for now, it all works.
     stupidCorners, stupidIds, cornersOfRejectedCandidates = cv.aruco.detectMarkers(image, tagFamily, parameters=detectorParameters)
-    if ((stupidIds is None) or len(stupidIds) == 0):
-        framePutter.putFrame(image)
-        return 0, 0, 0
+
+
+    corners = []
+    ids = []
+    if (stupidIds is None):
+        return corners, ids, image
     
     # unpack data in a format that actually makes sense
     # for some reason, they keep the actual values we want
     # within a list of size 1, so we pull them out here
     # so we don't have an annoying redundant index.
-    corners = []
-    ids = []
     for i in range(len(stupidIds)):
         actualId = stupidIds[i][0]
         actualCorners = stupidCorners[i][0]
         ids.append(actualId)
         corners.append(actualCorners)
+
+    for i in range(len(corners)):
+        fixedCorners = np.copy(corners[i])
+
+        fixedCorners[0] = corners[i][2]
+        fixedCorners[1] = corners[i][3]
+        fixedCorners[2] = corners[i][0]
+        fixedCorners[3] = corners[i][1]
+
+        corners[i] = fixedCorners
+
+        # put corners into right order convention
+        # so that axes are correct?
+
+
+    drawOnMe = image.copy()
+    annotatedTags = cv.aruco.drawDetectedMarkers(drawOnMe, stupidCorners, stupidIds)
+    return corners, ids, annotatedTags
+        
+
+
+
+
+
+
+
+def testOpenCVImpl(image, cameraMatrix):
+    tagWidthInches = 6.5 # 6.5 for 36h11, 6 for 16h5
+    metersPerInch = 0.0254
+    tagWidthMeters = tagWidthInches * metersPerInch
+
+    # corners is a list of 4x2 numpy arrays
+    corners, ids, drawOnMe = findTagCorners(image)
+    if (len(ids) == 0):
+        framePutter.putFrame(image)
+        return (0, 0, 0), np.zeros((3, 3))
     
     rotationVectors = [] # direction = rotation axis, magnitude = how many radians to rotate about that axis
     translationVectors = [] # units of meters
@@ -241,15 +368,7 @@ def testOpenCVImpl(image, cameraMatrix):
         translationVectors.append(translationVector)
 
 
-    # tagWidthInches = 6.5
-    # metersPerInch = 0.0254
-    # tagWidthMeters = tagWidthInches * metersPerInch
-    # distortionCoeffs = np.array([0, 0, 0, 0, 0], dtype=np.float32) # assume negligible lens distoriton
-    # rotationVectors, translationVectors, _ = cv.aruco.estimatePoseSingleMarkers(corners, tagWidthMeters, cameraMatrix, distortionCoeffs)
-
-
-    drawOnMe = image.copy()
-    drawOnMe = cv.aruco.drawDetectedMarkers(drawOnMe, stupidCorners, stupidIds)
+    # create viz
     howLongToDrawAxesMeters = tagWidthMeters
     for i in range(len(ids)):
         rotationVector = rotationVectors[i]
@@ -258,7 +377,9 @@ def testOpenCVImpl(image, cameraMatrix):
         # (x,y,z) -> (r,g,b)
     framePutter.putFrame(drawOnMe)
 
-
-    return translationVectors[0]
+    print("\nrotationVectors:", rotationVectors)
+    rotationMatrix, _ = cv.Rodrigues(rotationVectors[0])
+    print("rotationMatrix:", rotationMatrix)
+    return translationVectors[0], rotationMatrix
 
 main()